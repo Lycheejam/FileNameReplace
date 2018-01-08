0da2d1d311f7dd3acc7722162c9cc213af79a3ab
lycheejam <lycheejamwow@gmail.com>
2018-01-08 12:50:36 +0900

    ファイル一括置換の途中

--- FileNameReplace/Form1.Designer.cs
+++ FileNameReplace/Form1.Designer.cs
@@ -28,12 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.act_btn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.folder_btn = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // act_btn
+            // 
+            this.act_btn.Location = new System.Drawing.Point(13, 13);
+            this.act_btn.Name = "act_btn";
+            this.act_btn.Size = new System.Drawing.Size(259, 45);
+            this.act_btn.TabIndex = 0;
+            this.act_btn.Text = "実行";
+            this.act_btn.UseVisualStyleBackColor = true;
+            this.act_btn.Click += new System.EventHandler(this.act_btn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 19);
+            this.textBox1.TabIndex = 1;
+            // 
+            // folder_btn
+            // 
+            this.folder_btn.Location = new System.Drawing.Point(13, 64);
+            this.folder_btn.Name = "folder_btn";
+            this.folder_btn.Size = new System.Drawing.Size(258, 44);
+            this.folder_btn.TabIndex = 2;
+            this.folder_btn.Text = "フォルダ選択";
+            this.folder_btn.UseVisualStyleBackColor = true;
+            this.folder_btn.Click += new System.EventHandler(this.folder_btn_Click);
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(13, 140);
+            this.dgv.Name = "dgv";
+            this.dgv.RowTemplate.Height = 21;
+            this.dgv.Size = new System.Drawing.Size(259, 154);
+            this.dgv.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 306);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.folder_btn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.act_btn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button act_btn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button folder_btn;
+        private System.Windows.Forms.DataGridView dgv;
     }
 }
 
--- FileNameReplace/Form1.cs
+++ FileNameReplace/Form1.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace FileNameReplace
@@ -16,5 +12,73 @@ namespace FileNameReplace
         {
             InitializeComponent();
         }
+
+        private void folder_btn_Click(object sender, EventArgs e)
+        {
+            //FolderBrowserDialogクラスのインスタンスを作成
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            //上部に表示する説明テキストを指定する
+            fbd.Description = "フォルダを指定してください。";
+            //ルートフォルダを指定する
+            //デフォルトでDesktop
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+            //最初に選択するフォルダを指定する
+            //RootFolder以下にあるフォルダである必要がある
+            fbd.SelectedPath = @"C:\Windows";
+            //ユーザーが新しいフォルダを作成できるようにする
+            //デフォルトでTrue
+            fbd.ShowNewFolderButton = true;
+
+            //ダイアログを表示する
+            if (fbd.ShowDialog(this) == DialogResult.OK)
+            {
+                //選択されたフォルダを表示する
+                textBox1.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void act_btn_Click(object sender, EventArgs e)
+        {
+            //ワイルドカード"*"は、すべてのファイルを意味する
+            string[] files = Directory.GetFiles(
+                textBox1.Text, "*", SearchOption.AllDirectories);
+
+            var lists = new List<FileList>();
+            var regex = new Regex(@"正規表現");
+            foreach (var file in files)
+            {
+                var match = regex.Match(Path.GetFileName(file));
+                if (match.Success)
+                {
+                    lists.Add(new FileList
+                    {
+                        FullPath = Path.GetDirectoryName(file)
+                        , FileName = Path.GetFileName(file)
+                        , AfterName = Path.GetFileName(file).Replace("str", "")
+                    });
+                }
+            }
+
+            
+                foreach (var item in lists)
+                {
+                    File.Move(item.FullPath + "\\" + item.FileName,
+                              item.FullPath + "\\" + item.AfterName);
+                }
+            
+
+            //ネットで調べたら出てきた
+            //var bs = new BindingSource(lists, string.Empty);
+            //dgv.DataSource = bs;
+
+            dgv.DataSource = lists;
+        }
+    }
+    class FileList
+    {
+        public string FullPath { get; set; }
+        public string FileName { get; set; }
+        public string AfterName { get; set; }
     }
 }
